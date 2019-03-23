e02c55f46c076f0cceb4ae7b1a01de8c5bd73739
lemnver <lemnver@github.com>
2019-03-23 23:55:22 +0100

    changed package directory to lowercase, created main class, added bash files to gitignore

---
+++ src/bordell/MainClass.java
@@ -0,0 +1,11 @@
+package bordell;
+import bordell.graphics.Frame;
+
+public class MainClass {
+    
+    public static void main (String[] args){
+        Frame bordellFrame = new Frame();
+
+        bordellFrame.start();
+    }
+}
--- src/bordell/graphics/Frame.java
+++ src/bordell/graphics/Frame.java
@@ -2,5 +2,10 @@ package Bordell.graphics;
 import jawax.swing.JFrame;
 
 public class Frame extends JFrame {
+
+    public Frame(){
+    
+    }
+    public void start(){}
     
 }
